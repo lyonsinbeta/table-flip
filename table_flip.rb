eeb35eae1e51212592a4b1cb3777e1ebc12b6798
David Lyons <lyonsinbeta@gmail.com>
2014-09-09 16:09:19 -0600

    Makes custom flips work, but it's not ideal

--- table_flip.rb
+++ table_flip.rb
@@ -125,7 +125,7 @@ FLIP = {
 			slack_client = make_client(params)
 		  if params["text"].empty?
 				slack_client.notify(FLIP[:flipping])
-			elsif FLIP.include? "/#{params["text"].downcase.to_sym}"
+			elsif FLIP.include? params["text"].downcase.to_sym
 				slack.client.notify(FLIP[params["text"].downcase.to_sym])
 			else
 				slack_client.notify("(╯°□°)╯︵ #{flip_word(params["text"])}")
